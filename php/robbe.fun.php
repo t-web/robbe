d459f17558b653843f110b366754eaf0fc7218b2
狮子的魂 <chenxin619315@gmail.com>
2013-11-24 18:02:33 +0800

    最新rb_split测试函数, 添加配置样板和使用说明

--- php/robbe.fun.php
+++ php/robbe.fun.php
@@ -12,12 +12,16 @@ echo "rb_dic_exist(研究) ? ".rb_dic_exist(__RB_LEX_CJK_WORDS__, "研究")."<br
 
 $_entry = rb_dic_get(__RB_LEX_CJK_WORDS__, "你");
 echo "rb_dic_get(你)：<br />";
-echo "|--length: ".$_entry["length"].", freq: ".$_entry["freq"]."<br /><br />";
+echo "|——length: ".$_entry["length"].", freq: ".$_entry["freq"]."<br /><br />";
 
 echo "分词函数：<br />";
-$__str__ = "robbe高性能php中文分词组件。";
-echo "rb_split(" . $__str__ . ")：<br />";
-$_result = rb_split($__str__, __RB_COMPLEX_MODE__);
+$__str__ = "研究生命起源,robbe高性能php中文分词组件。";
+echo "rb_split(\"" . $__str__ . "\")：<br />";
+//完整的配置: 
+//array('max_len'=>5, 'r_name'=>0, 'mix_len'=>2, 'lna_len'=>1, 'add_syn'=>1, 'clr_stw'=>1, 'keep_urec'=>0, 'spx_out'=>0, 'nthreshold'=>1000000, 'mode'=>__RB_COMPLEX_MODE__;
+//1.在不了解friso内核的情况下, 请不要随便更改nthreshold
+//2.使用NULL来使用php.ini中指定的friso.ini文件中的配置
+$_result = rb_split($__str__, array('mode'=>__RB_COMPLEX_MODE__, 'spx_out'=>1));
 foreach ( $_result as $_value ) {
 	echo $_value."/ ";
 }
