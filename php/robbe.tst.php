b298649bc3216d59b7e1f348ec255dcabb2543b1
狮子的魂 <chenxin619315@gmail.com>
2013-11-25 17:46:55 +0800

    修改robbe分词测试程序, 加入配置选项

--- php/robbe.tst.php
+++ php/robbe.tst.php
@@ -1,20 +1,22 @@
 <?php
 header('content-type:text/html;charset:utf-8');
+ini_set('magic_quotes_gpc', 0);
 
-$__text__ = '';
-$__mode__ = 2;
-$__timer__ = 0;
+$text = '';
+$_timer = 0;
 $_act = '';
-if ( isset($_POST['_act']) ) {
-	$_act = $_POST['_act'];
-	if ( $_act == 'split'  ) {
-		$__text__ = $_POST['text'];
-		$__mode__ = intval( isset($_POST['mode'])?$_POST['mode']:1 );
-
-		$s_time = timer();
-		$_result = rb_split($__text__, $__mode__);
-		$__timer__ = timer() - $s_time;
-	}
+$_cfg = array('mode' => __RB_COMPLEX_MODE__);
+if ( isset($_POST['_act']) && ($_act = $_POST['_act']) == 'split' ) {
+	$text = &$_POST['text'];
+	$_cfg = &$_POST['config'];
+	if ( ! isset($_cfg['add_syn']) )	$_cfg['add_syn'] = 0;
+	if ( ! isset($_cfg['clr_stw']) )	$_cfg['clr_stw'] = 0;
+	if ( ! isset($_cfg['keep_urec']) )	$_cfg['keep_urec'] = 0;
+	if ( ! isset($_cfg['spx_out']) )	$_cfg['spx_out'] = 0;
+	
+	$s_time = timer();
+	$_ret = rb_split($text, $_cfg);
+	$_timer = timer() - $s_time;
 }
 
 function timer() {
@@ -30,7 +32,7 @@ function timer() {
 	<title>robbe分词测试程序</title>
 	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
 	<style type="text/css">
-		#box {width: 1000px;}
+		#box {width: 1000px}
 		.input-text {border: 1px solid #CCC;width: 1000px;height: 200px;background-color: #FFF;
 			color: #555;font-size: 14px;}
 		.link-box {overflow: hidden;zoom:1;padding-top:10px;}
@@ -43,33 +45,67 @@ function timer() {
 		.link-item {float: left;font-size: 14px;font-weight: bold;
 			height: 26px;line-height: 26px;width: 100px;color: #A50100;}
 		.title-item {height:30px;line-height: 30px;font-size: 14px;font-weight: bold;}
+		
+		#cfg-box {margin-bottom: 10px;}
+		#cfg-box div {overflow: hidden;zoom:1;color:#555;font-size:12px;}
+		#cfg-box div label {float: left;width: 150px;height: 26px;line-height:26px;text-align:right;
+			padding-right:10px;font-size:12px;font-weight:bold;color:#555;}
+		.input {border: 1px solid #DDD;height: 18px;line-height: 18px;padding-left: 5px;width: 60px;
+			color:#555; outline: none;}
 	</style>
 </head>
 
 <body>
 	<div id="box">
-		<div class="title-item">请输入分词内容：</div>
 		<form name="robbe" method="post" action="robbe.tst.php">
-			<div class="r-item"><textarea name="text" class="input-text" id="text"><?php echo $__text__;?></textarea></div>
-			<input type="hidden" name="_act" value="split"/>
-			<div class="link-box">
-			<a class="link-item">
-				<input type="radio" name="mode" value="1" <?php echo $__mode__==1?'checked="checked"':'';?>/>简易模式</a>
-			<a class="link-item">
-				<input type="radio" name="mode" value="2" <?php echo $__mode__==2?'checked="checked"':'';?>/>复杂模式</a>
-			<a href="javascript:;" onclick="do_submit();return false;" id="submit-link">robbe分词</a>
+		<div class="title-item">分词配置：</div>
+		<div id="cfg-box">
+			<div>
+				<label>最大词长: </label>
+				<input type="text" name="config[max_len]" value="<?=isset($_cfg['max_len'])?$_cfg['max_len']:5?>" class="input" />
+			</div>
+			<div>
+				<label>混合词中文词长: </label>
+				<input type="text" name="config[mix_len]" value="<?=isset($_cfg['mix_len'])?$_cfg['mix_len']:2?>" class="input" />
+			</div>
+			<div>
+				<label>同义词追加: </label>
+				<input type="checkbox" name="config[add_syn]" <?=isset($_cfg['add_syn'])&&$_cfg['add_syn']==1?'checked="checked"':''?> value="1" />
+			</div>
+			<div>
+				<label>过滤停止词: </label>
+				<input type="checkbox" name="config[clr_stw]" <?=isset($_cfg['clr_stw'])&&$_cfg['clr_stw']==1?'checked="checked"':''?> value="1" />
+			</div>
+			<div>
+				<label>保留未识别词: </label>
+				<input type="checkbox" name="config[keep_urec]" <?=isset($_cfg['keep_urec'])&&$_cfg['keep_urec']==1?'checked="checked"':''?> value="1" />
+			</div>
+			<div>
+				<label>sphinx定制输出: </label>
+				<input type="checkbox" name="config[spx_out]" <?=isset($_cfg['spx_out'])&&$_cfg['spx_out']==1?'checked="checked"':''?> value="1" />
+			</div>
+			<div>
+				<label>分词模式: </label>
+				<input type="radio" name="config[mode]" value="<?=__RB_SIMPLE_MODE__?>" <?=isset($_cfg['mode'])&&$_cfg['mode']==1?'checked="checked"':''?> />简易模式
+				<input type="radio" name="config[mode]" value="<?=__RB_COMPLEX_MODE__?>" <?=isset($_cfg['mode'])&&$_cfg['mode']==2?'checked="checked"':''?> />复杂模式
+			</div>
 		</div>
+		
+		<div class="title-item">分词内容：</div>
+		<div class="r-item"><textarea name="text" class="input-text" id="text"><?=$text?></textarea></div>
+		<input type="hidden" name="_act" value="split"/>
+		<a href="javascript:;" onclick="do_submit();return false;" id="submit-link">robbe分词</a>
 		</form>
 
 		<?php
 		if ( $_act == 'split' ) {
 		?>
-		<div class="title-item">robbe分词结果：</div>
-		<div><textarea class="input-text"><?php foreach ( $_result as $_value ) echo $_value.' ';?>
+		<div class="title-item">分词结果：</div>
+		<div><textarea class="input-text"><?php foreach ( $_ret as $_value ) echo $_value.' ';?>
 		</textarea></div>
 		<div class="link-box"><a id="info-link">
 			<?php
-			$len = strlen($__text__);
+			$len = strlen($text);
 			if ( $len >= 1048576 ) {
 				echo substr(($len/1048576), 0, 6).'MB';
 			} else if ( $len >= 1024 ) {
@@ -78,7 +114,7 @@ function timer() {
 				echo $len.'B';
 			}
 			?>
-			&nbsp;&nbsp;&nbsp;<?php printf("%.5f", $__timer__)?>sec
+			&nbsp;&nbsp;&nbsp;<?php printf("%.5f", $_timer)?>sec
 		</a></div>
 		<?php
 		}
